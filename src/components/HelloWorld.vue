bd6bf2b056e2d277fa4e9a311f9e5bab6367f98f
zxfjd3g <258147149@qq.com>
2019-03-09 16:39:43 +0800

    init app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        title: '第一个组件标题2222'
+      }
+    }
+  }
+</script>
+<style scoped>
+  h2 {
+    color: red;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>'
+})
